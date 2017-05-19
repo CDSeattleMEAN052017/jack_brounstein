cab0db5458c67e5898a28d869812ccb96daba0b1
Jack Brounstein <Jack.Brounstein@yahoo.com>
2017-05-19 12:33:57 -0700

    adds full MEAN demo

---
+++ angular/full_mean_demo/client/src/app/books/books-list/books-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Book } from "./../book"
+
+@Component({
+  selector: 'app-books-list',
+  templateUrl: './books-list.component.html',
+  styleUrls: ['./books-list.component.css']
+})
+export class BooksListComponent implements OnInit {
+  @Input() books: Book[]
+  chosen_book: Book
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+  choose_book(book: Book){
+    if(book == this.chosen_book){
+      this.chosen_book = undefined
+    } else {
+      this.chosen_book = book
+    }
+  }
+
+}
